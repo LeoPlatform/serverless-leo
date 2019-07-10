94e33c84673ed21850091381e077ea4080728b4d
Joseph Lee Hunsaker <joseph.l.hunsaker@gmail.com>
2019-07-09 22:10:47 -0600

    Break out serverless-leo-tools
    The tools will have a dependency on leo-sdk, which is a bit much for just the vanilla serverless-leo. Plus it's probably good to break out these commands anyway.

--- src/tools/add-npm-scripts.js
+++
@@ -1,2 +0,0 @@
-// this will add the approprate npm scripts for the type of package.json
-// (system, microservice, bot, etc..)
--- src/tools/remove-triggers.js
+++
@@ -1,9 +0,0 @@
-// should mark a bot "unscheduled" i.e. will never be triggered
-/*
-{
-	"id": "bot:dw-domain-lead",
-	"name": "dw-domain-lead",
-	"triggers": null,
-	"time": null,
-}
-*/
